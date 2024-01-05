e89b076e0c14f3e1616dfa2677d8c3d962786b9b
sjq1412 <sarahdev.cod3@gmail.com>
2024-01-05 10:08:15 +0800

    implement displaying todos and adding todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
-function App() {
-  return <>app</>;
-}
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const App = () => {
+  const [todos, setTodos] = useState<string[]>([]);
+  const [newTodo, setNewTodo] = useState<string>('');
+
+  const handleAddTodo = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+
+    const sanitizedText = newTodo.trim();
+    if (sanitizedText) {
+      setTodos((prevState) => prevState.concat(sanitizedText));
+      setNewTodo('');
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleAddTodo}>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button type="submit" disabled={!newTodo}>
+          Add
+        </button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={uuidv4()}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
